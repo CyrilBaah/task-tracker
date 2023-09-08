2bc2931b78536efa0fdac021a211bb328a7ac91e
CyrilBaah <cbaah123@gmail.com>
2023-09-08 10:07:46 +0000

    linters and formatters

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 
 @Module({
-  controllers: [TasksController]
+  controllers: [TasksController],
 })
 export class TasksModule {}
 
-
 export class Task {
-    id: number;
-    title: string;
-  }
-  
+  id: number;
+  title: string;
+}
