c96da7eb4e16d6d62a178c78de1c071f23aa0cd5
CyrilBaah <cbaah123@gmail.com>
2023-09-18 14:30:21 +0000

    fix test

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    return JSON.stringify({
-      success: true,
-      message: "Task Tracker",
-      cicd: false,
-      branch: "stage"
-    });
+    return 'Hello World!';
   }
 }
